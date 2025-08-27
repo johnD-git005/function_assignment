a176b1f3c01379bca2e72a866ab7388527691d29
johnD-git005 <johndakshak@gmail.com>
2025-08-27 22:18:27 +0100

    Initial commit

--- question4.py
+++ question4.py
@@ -2,12 +2,9 @@
 Area=1/2×base×height
 Example: triangle_area(10, 5) → 25.0 '''
 
-base = float(input("Base Of Triangle: "))
-height = float(input("Height of Triangle: "))
-
-def triangle_area():
+def triangle_area(base, height):
 
 	result = 1/2 * base * height
 	print(f"Area →  {result}")
 
-triangle_area()
+triangle_area(10, 5)
