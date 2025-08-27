53d4910589a7e8a7128af44dec5d7c26da5297bb
johnD-git005 <johndakshak@gmail.com>
2025-08-27 21:37:51 +0100

    Initial commit

--- question1.py
+++ question1.py
@@ -1,14 +1,12 @@
 ''' Write a function check_even_odd(n) that takes an integer n and returns:
 "Even" if the number is even,
- "Odd" if the number is odd. '''
+"Odd" if the number is odd. '''
 
-def check_even_odd():
-	number = int(input("Enter a Number: "))
-	if number % 2 == 0:
+def check_even_odd(n):
+	if n % 2 == 0:
 		print("Even Number")
 	else: 
 		print("Odd Number")
 
-check_even_odd()
- 	
-
+number = int(input("Enter a Number: "))
+check_even_odd(number)
